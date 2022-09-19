043e3e6ba9ddeed567282d42595421b8bbc2a6f4
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-19 00:09:51 -0600

    last commit tn, could not solve my index.js route issue

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const categoryRoutes = require("./api/category-routes");
+const productRoutes = require("./api/product-routes");
+const tagRoutes = require("./api/tag-routes");
 
-router.use("/api", apiRoutes);
-
+// router.use("/api", apiRoutes);
+router.use("/api/category-routes", categoryRoutes);
+router.use("/api/product-routes", productRoutes);
+router.use("/api/tag-routes", tagRoutes);
+// console.log(apiRoutes);
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
 });
